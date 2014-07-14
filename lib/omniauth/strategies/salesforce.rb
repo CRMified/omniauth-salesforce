05ccd9be27361e77dcba6fd38d603d2eb0444416
mohamed ibrahim <mohamed.mahmoud59@gmail.com>
2014-07-14 20:00:25 +0100

    Update salesforce.rb

--- lib/omniauth/strategies/salesforce.rb
+++ lib/omniauth/strategies/salesforce.rb
@@ -4,12 +4,12 @@ require 'base64'
 
 module OmniAuth
   module Strategies
-    class SalesforceSandboxMo < OmniAuth::Strategies::OAuth2
+    class Salesforce < OmniAuth::Strategies::OAuth2
 
       MOBILE_USER_AGENTS =  'webos|ipod|iphone|ipad|android|blackberry|mobile'
 
       option :client_options, {
-        :site          => 'https://test.salesforce.com',
+        :site          => 'https://login.salesforce.com',
         :authorize_url => '/services/oauth2/authorize',
         :token_url     => '/services/oauth2/token'
       }
@@ -81,18 +81,5 @@ module OmniAuth
       end
 
     end
-
-    class SalesforceSandbox < OmniAuth::Strategies::Salesforce
-      default_options[:client_options][:site] = 'https://test.salesforce.com'
-    end
-
-    class DatabaseDotCom < OmniAuth::Strategies::Salesforce
-      default_options[:client_options][:site] = 'https://login.database.com'
-    end
-
-    class SalesforcePreRelease < OmniAuth::Strategies::Salesforce
-      default_options[:client_options][:site] = 'https://prerellogin.pre.salesforce.com/'
-    end
-
   end
 end
